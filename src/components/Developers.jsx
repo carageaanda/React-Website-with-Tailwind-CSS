41bf4ab8bbbd6f36ff808450c13caa050cbfc669
carageaanda <anda.caragea99@gmail.com>
2022-09-21 23:06:29 +0300

    react

---
+++ src/components/Developers.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import terminal from '../assets/terminal.png'
+
+const Developers = () => {
+  return (
+    <div className='w-full text-white bg-black flex justify-center'>
+        <div className='max-w-[1240px] px-4 py-16 md:flex'>
+         <div>
+            <h1>Superpowers for DEFI developers.</h1>
+            <p>
+                Check out the <span className='blue'>documentation</span>, the <span className='blue'>quick start</span> or a guide below to
+                integrate your project with thousands of tokens and billions of
+                liquidity.
+            </p>
+        </div>
+         <div className='flex justify-center w-full py-16'>
+            <img className='max-w-[350px] shadow-lg shadow-cyan-500/50' 
+            src={terminal} alt="/"/>
+         </div>
+        </div>
+
+    </div>
+  );
+};
+
+export default Developers;
